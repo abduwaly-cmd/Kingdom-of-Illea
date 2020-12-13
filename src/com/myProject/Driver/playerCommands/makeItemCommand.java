ec8209722b1ae84e8cf42cbc2d17f1644665d86a
Abdulrhman <b00078162@aus.edu>
2020-12-13 19:16:53 +0400

    Update makeItemCommand.java

--- src/com/myProject/Driver/playerCommands/makeItemCommand.java
+++ src/com/myProject/Driver/playerCommands/makeItemCommand.java
@@ -11,14 +11,13 @@ import java.io.IOException;
 public class makeItemCommand implements Command {
     @Override
     public void execute(Player player, String[] act) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
-        if(act[1].equals("healing potion")) {
-            player.addItem(new healingPotion());
-            player.getLocation().talk();
+        if(act[1].equals("healing potion")) player.addItem(new healingPotion());
+        else if(act[1].equals("cleansing potion")) player.addItem(new cleansingPotion());
+        else {
+            System.out.println("Invalid item :/");
+            return;
         }
-        else if(act[1].equals("cleansing potion")){
-            player.addItem(new cleansingPotion());
-            player.getLocation().talk();
-        }
+        player.getLocation().talk();
     }
 
     @Override
